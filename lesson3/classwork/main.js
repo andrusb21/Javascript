700b5427ac2fcb6349d9d551395b93da507a0e56
andrusb21 <andrusyshyn1996@gmail.com>
2021-11-20 17:45:50 +0200

    hw3 end

---
+++ lesson3/classwork/main.js
@@ -0,0 +1,60 @@
+ let num = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+
+
+// let i = 0;
+// while (i < num.length) {
+//     console.log(num[i])
+//     i++;
+// }
+
+
+// for (let number of num) {
+//     console.log(number)
+//
+// }
+
+ // 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+ // let i = 0;
+ // while (i < num.length) {
+ //     if (i%2 ===1){
+ //     console.log([i])}
+ //
+ //     i++;
+ // }
+ // 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+ // for (let i = 0; i < num.length; i++){
+ //     if (i%2 !== 0){
+ //         console.log(num[i]);
+ //     }
+ // };
+ // 5. перебрати циклом while та вивести  числа тільки парні  значення
+ // i = 0;
+ // while (i < num.length){
+ //     if (num[i]%2 === 0){
+ //         console.log(num[i]);
+ //     };
+ //     i++
+ // };
+ // 6. перебрати циклом for та вивести  числа тільки парні  значення
+ // for (let number of num) {
+ //     if (number%2===0) {
+ //     console.log(number)}
+ //
+ // }
+ // 7. замінити кожне число кратне 3 на слово "okten"
+
+ // for (let item of num){
+ //     if (item % 3 === 0){
+ //         item='okten';
+ //         }
+ //     console.log(item);
+ //     }
+
+ // // 8. вивести масив в зворотньому порядку.
+
+ // for (let i = num.length-1; i >= 0; i--) {
+ //     console.log(num[i]);
+ // }
+
+
