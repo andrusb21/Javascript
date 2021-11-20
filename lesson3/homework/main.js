2da5ec212cc9000dcae66c289a769a975229c414
andrusb21 <andrusyshyn1996@gmail.com>
2021-11-20 16:43:00 +0200

    hw3 end

--- lesson3/homework/main.js
+++ lesson3/homework/main.js
@@ -71,3 +71,62 @@
 //         console.log(arrayNumber[i]);
 //     }
 // }
+// let arrayNumber = [false, 'two', 1996, true, 'okten', 23, 45, 'vasia', {}, [], 'hi'];
+// for (i = 0; i < arrayNumber.length; i++) {
+//     if (typeof arrayNumber[i] === 'number') {
+//         console.log(arrayNumber[i]);
+//     }
+// }
+
+// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів.
+// Вивести в консоль всі його елементи в циклі.
+// let emptyArray = [];
+// emptyArray[0] = 'Hello';
+// emptyArray[1] = 'new';
+// emptyArray[2] = 'year';
+// emptyArray[3] = 20;
+// emptyArray[4] = 22;
+// emptyArray[5] = true;
+// emptyArray[6] = false;
+// emptyArray[7]= {};
+// emptyArray[8]= [];
+// emptyArray[9]='!';
+// for (let x of emptyArray) {
+//     console.log(x)
+// }
+// Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+// for (let i = 0; i < 10; i++) {
+//     console.log('поточний ' + i + ' номер')
+//     document.write('поточний ' + i + ' номер')
+// }
+
+
+// for (let i = 0; i < 100; i++) {
+//     console.log('поточний ' + i + ' номер')
+//     document.write('поточний ' + i + ' номер')
+// }
+
+// - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i < 100; i+=2) {
+//     console.log('поточний крок циклу : ' + i + ' ');
+//     document.write('поточний крок циклу : ' + i + ' ')
+
+// - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+
+// for (let i = 0; i <100 ; i++) {
+//     if (i%2 === 0) {
+//         console.log('поточний крок циклу : ' + i + ' ');
+//         document.write('поточний крок циклу : ' + i + ' ')
+//     }
+// }
+
+// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+
+for (let i = 0; i <100 ; i++) {
+    if (i%2 === 1) {
+        console.log('поточний крок циклу : ' + i + ' ');
+         document.write('поточний крок циклу : ' + i + ' ')
+    }
+
+}
