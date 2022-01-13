34f35a2a16b6895859db1a9e3a0bd81bf4d0176d
andrusb21 <andrusyshyn1996@gmail.com>
2022-01-13 20:59:20 +0200

    hw cw 8

--- lesson9/homework/main9.js
+++ lesson9/homework/main9.js
@@ -1,39 +1,81 @@
-// // let str = '+380 96 - 52 - 42 - 457.';
-// // let s = str.replaceAll(' ', '').replaceAll('-', '').replaceAll('.','');
-// // console.log(s);
-// // console.log(typeof (s))
-// // let s1 = str.toUpperCase();
+// Все робити за допомоги js.
+// - створити блок,
+//     - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+
+// let div = document.createElement('div');
+// div.classList.add('wrap', 'collapse', 'alpha', 'beta');
+// div.style.background = 'blue';
+// div.style.color = 'yellow';
+// div.style.fontSize = '40px';
+// div.innerText = 'UKRAINE';
+// div.style.width = '300px';
+// document.body.appendChild(div);
+
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// Завдання робити через цикли.
+
+let arr = ['Main', 'Products', 'About us', 'Contacts'];
+let menu = document.getElementsByClassName('menu')[0];
+for (const item of arr) {
+    let li = document.createElement('li')
+    li.innerText = item;
+    menu.appendChild(li)
+
+}
+
 //
-// // let arr = [11,22,33, [1,2,3],'asd'];
-// // let map = arr.map(value => console.log(value));
-// - Знайти та вивести довижину настипних стрінгових значень
-// 'hello world', 'lorem ipsum', 'javascript is cool'
-let h1 = 'hello world';
-let h1lenght= h1.length;
-console.log(h1lenght)
-// - Перевести до великого регістру наступні стрінгові значення
-// 'hello world', 'lorem ipsum', 'javascript is cool'
-// - Перевести до нижнього регістру настипні стрінгові значення
-// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
-// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+for (const one of coursesAndDurationArray) {
+    let div = document.createElement('div')
+    div.innerText = one.title + ' ' +  one.monthDuration
+    document.body.appendChild(div)
+}
+
 //
-//
-// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
-    let str = 'hello';
-let H = str[0].toUpperCase() + str.slice(1)
-console.log(H);
-//
-//
-// - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
-//     let str = 'Каждый охотник желает знать';
-// document.writeln(delete_characters(str, 7)); // Каждый
-//
-// - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
-//     let str = "HTML JavaScript PHP";
-// document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
-//
-// - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
-//
-//
-//
-// - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
+for (const item1 of coursesAndDurationArray) {
+    let div1 = document.createElement('div');
+    div1.classList.add('item');
+    let h1 = document.createElement('h1');
+    h1.classList.add('heading');
+    h1.innerText = item1.title
+    let p = document.createElement('p');
+    p.classList.add('description');
+    p.innerText = item1.monthDuration;
+    div1.append(h1,p)
+    document.body.appendChild(div1);
+}
